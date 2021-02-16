24be5c1643faeb75a5c6b16c6f13b3b483f6bcbc
yourname <youremail@address.com>
2021-02-15 23:07:55 -0500

    changes

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -1,26 +1,18 @@
 """ This set of functions that should return the appropriate data type"""
-
 def return_number_3():
     """ This function should return an integer with the value of 3"""
-
     return_value = 3
     return return_value
-
-
 def return_string_vcu():
     """ This function should return a string with the lowercase value of vcu"""
-
     return_value = "VCU".lower()
     return return_value
-
 input_string = "HII"
 def return_lowercased_string(input_string):
     """You have a variable called input_string that is of type string.
     Return it but the lowercase version of it."""
-
     return_value = input_string.lower()
     return return_value
-
 input_string = "    HI    "
 def return_without_starting_ending_whitespace(input_string):
     """You have a variable called input_string that is of type string.
@@ -28,11 +20,9 @@ def return_without_starting_ending_whitespace(input_string):
 
     return_value = input_string.strip()
     return return_value
-
 first_number = 100
 second_number = 444
 def return_addition(first_number, second_number):
     """ Return the two numbers added together. """
-
     return_value = first_number + second_number
     return return_value
